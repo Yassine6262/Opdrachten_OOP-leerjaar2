44378e5f4cace8a18c91b62200527f7060989f6f
Yassine6262 <112876986+Yassine6262@users.noreply.github.com>
2024-04-03 09:06:49 +0200

    8

---
+++ opdracht8_OOP/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gebruiker/opdrachten-oo-pleerjaar2',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '83ad59d92b373afb8fe54c55f3151287d36c3467',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gebruiker/opdrachten-oo-pleerjaar2' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '83ad59d92b373afb8fe54c55f3151287d36c3467',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
